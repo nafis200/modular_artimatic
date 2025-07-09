6bdbad425b516661738ca2b8d263da36dcde065b
nafis200 <nafisahamed14@gmail.com>
2025-07-09 15:36:10 +0600

    big mod

---
+++ modex_uva.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long Binary_exponential(long long x,long long y, long long mod){
+     long long result = 1;
+     x = x % mod;
+     while(y > 0){
+        if(y % 2 == 1){
+            result = ((result % mod) * (x % mod)) % mod;
+        }
+        x = ((x % mod) * (x % mod)) % mod;
+        y = y / 2;
+     }
+     return result;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long x, y, z;
+        cin >> x >> y >> z;
+        cout << Binary_exponential(x,y,z) << "\n";
+    }
+}
